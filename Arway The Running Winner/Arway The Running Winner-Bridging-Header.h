52c92ba98131cf5f834c8218c6c0218a6a71c79e
Anthony Boutinov <anton4488@gmail.com>
2015-04-17 23:12:34 +0300

    Sparkle Framework (auto update)

--- Arway The Running Winner/Arway The Running Winner-Bridging-Header.h
+++ Arway The Running Winner/Arway The Running Winner-Bridging-Header.h
@@ -4,3 +4,12 @@
 
 #import "JSTileMap.h"
 #import "LFCGzipUtility.h"
+//#import "Sparkle.framework/Sparkle.h"
+//#import "Sparkle.framework/SUAppcast.h"
+//#import "Sparkle.framework/SUAppcastItem.h"
+//#import "Sparkle.framework/SUError.h"
+//#import "Sparkle.framework/SUExport.h"
+//#import "Sparkle.framework/SUStandardVersionComparator.h"
+//#import "Sparkle.framework/SUUpdater.h"
+//#import "Sparkle.framework/SUVersionComparisonProtocol.h"
+//#import "Sparkle.framework/SUVersionDisplayProtocol.h"
